405b8dc7202b2a73b748b9a060b0958b78aa711d
Edward Carmack <AriemethAcc@gmail.com>
2017-01-17 23:35:09 -0500

    Updated to use the quantum pulse project for the engine.

--- engine/entity.go
+++
@@ -1,6 +0,0 @@
-package engine
-
-// Entity represents the basic behaviors all game objects have.
-type Entity interface {
-	ID() string
-}
--- engine/renderable.go
+++
@@ -1,8 +0,0 @@
-package engine
-
-// Renderable represents an object that can be rendered
-type Renderable interface {
-	Entity
-	Update(elapsed float64)
-	Render()
-}
